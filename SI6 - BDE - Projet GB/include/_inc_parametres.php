af27f2b4da9561beef92f7743ae8a1b90ba97071
antoinelepage <37927989+antoinelepage@users.noreply.github.com>
2018-03-30 11:46:36 +0200

    connexionbdd

--- SI6 - BDE - Projet GB/include/_inc_parametres.php
+++ SI6 - BDE - Projet GB/include/_inc_parametres.php
@@ -9,5 +9,5 @@ $HOTE = "localhost";	// nom du serveur de données : localhost ou serv-wamp1 ou 
 $PORT = '3306';			// numéro du port
 $USER = "root";			// nom de l'utilisateur
 $PWD  = "root";		// son mot de passe					
-$BDD  = "dlsbdeProjetA";		// nom de la base de données	
+$BDD  = "dlsbdeProjetB";		// nom de la base de données	
 ?>
